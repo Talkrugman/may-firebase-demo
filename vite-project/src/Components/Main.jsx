81a547b602f5743a5ba90dfb3cef15b62092ebf5
tal krugman <talkrugman@gmail.com>
2023-07-02 12:59:47 +0300

    new somthing

--- vite-project/src/Components/Main.jsx
+++ vite-project/src/Components/Main.jsx
@@ -1,6 +1,6 @@
 function Main() {
     return <main>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident deserunt voluptatibus, eum perferendis doloremque aut, explicabo amet mollitia asperiores esse consectetur consequatur sunt laborum natus odit impedit eius nostrum nisi.
+        onounobudskjfbdklhb
     </main>
 }
 
